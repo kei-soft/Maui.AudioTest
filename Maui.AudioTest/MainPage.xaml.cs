f08f1b51d2d89589fc72952fe737e2e3fec639b2
kjun <keisoft.kjun@gmail.com>
2024-05-10 07:23:21 +0900

    초기 커밋.

---
+++ Maui.AudioTest/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Maui.AudioTest
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainViewModel();
+        }
+    }
+}
